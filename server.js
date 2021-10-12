f4ad93c7d6246bac433d308ae8b74457d75f82ec
unknown <2002anuragksingh@gmail.com>
2021-10-12 23:02:14 +0530

    Add error 404  page

--- server.js
+++ server.js
@@ -1,26 +1,53 @@
+
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 
 
+app.use(bodyParser.urlencoded({ extended: true }))
 
+// Including static files - CSS and JS
 app.use(express.static('public'))
 
-app.get("/", function(req,res){
-    res.sendFile(__dirname+"/view/login/login.html")
+//home route - sign up
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/view/login/login.html")
 })
-app.get("/chat", function(req,res){
-    console.log(req.url);
-    res.sendFile(__dirname+"/view/chat/index.html")
+
+//chat route
+app.get("/chat", function (req, res) {
+
+    res.sendFile(__dirname + "/view/chat/index.html")
+})
+
+//error route
+app.get("/error", function (req, res) {
+
+    res.sendFile(__dirname+"/view/error/index.html")
 })
 
 
 // Redirecting from login to chat
-app.post("/", function(req,res){
-    res.redirect("/chat")
+
+
+
+app.post("/", function (req, res) {
+    if (req.body.PASSWORD == "fourmuskeeters" || req.body.PASSWORD == 100229) {
+        res.redirect("/chat")
+
+    }
+    else{
+        res.redirect("/error")
+    }
+    
 })
 
 
+app.post("/error", function (req, res) {
+    res.redirect("/")
+})
+
 //server created
 app.listen(3000, function () {
-    console.log("Server running on PORT 3000");
-  })
+    console.log("Server running on PORT 3000.");
+})
