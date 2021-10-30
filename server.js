786197c1e920fb777c492763df79185d29524ec8
unknown <2002anuragksingh@gmail.com>
2021-10-30 16:34:01 +0530

    Scholar Number

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get("/chat", function (req, res) {
     res.render('chat', {
         name: app.get('name_var'), 
         linkedin: app.get('linkedin_var'), 
-        twitter: app.get('twitter_var'),
+        user: app.get('user_var'),
         github: app.get('github_var')});
 })
 
@@ -35,7 +35,7 @@ app.post("/", function (req, res) {
  
     app.set('name_var', req.body.Name);            // In "/" route store variable for "/chat" route
     app.set('linkedin_var', req.body.Linkedin);
-    app.set('twitter_var', req.body.Twitter);
+    app.set('user_var', req.body.User);
     app.set('github_var', req.body.Github);
 
     if (req.body.PASSWORD == "fourmuskeeters" || req.body.PASSWORD == 100229) {
