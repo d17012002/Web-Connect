30d0b77bfeabb9fa506cd6acf56f38df16dcc948
unknown <2002anuragksingh@gmail.com>
2021-12-03 14:37:24 +0530

    Login form update

--- server.js
+++ server.js
@@ -1,168 +1,173 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
-const http = require('http');
-const socketio = require('socket.io');
-const formatMessage = require('./utils/messages');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
 const {
   userJoin,
   getCurrentUser,
   userLeave,
-  getRoomUsers
-} = require('./utils/users');
+  getRoomUsers,
+} = require("./utils/users");
+const { PassThrough } = require("stream");
 
-
-
-const app = express()
+const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Including static files - CSS and JS
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 //home route - sign up
 app.get("/", function (req, res) {
-    res.render("login")
-})
+  res.render("login");
+});
 
 //chat route
 app.get("/chat", function (req, res) {
-    res.render('chat', {
-        name: app.get('name_var'),
-        linkedin: app.get('linkedin_var'),
-        user: app.get('user_var'),
-        github: app.get('github_var')
-    });
-})
+  res.render("chat", {
+    name: app.get("name_var"),
+    linkedin: app.get("linkedin_var"),
+    user: app.get("user_var"),
+    github: app.get("github_var"),
+  });
+});
 
 //Video call route
 app.get("/videocall", function (req, res) {
-    res.render('videocall', {
-        name: app.get('name_var'),
-    });
-})
-
-
-
+  res.render("videocall", {
+    name: app.get("name_var"),
+  });
+});
 
 //error route
 app.get("/error", function (req, res) {
-
-    res.render("error")
-})
+  res.render("error");
+});
 
 app.post("/", function (req, res) {
+  app.set("name_var", req.body.Name); // In "/" route store variable for "/chat" route
+  app.set("linkedin_var", req.body.Linkedin);
+  app.set("user_var", req.body.User);
+  app.set("github_var", req.body.Github);
 
-    app.set('name_var', req.body.Name);            // In "/" route store variable for "/chat" route
-    app.set('linkedin_var', req.body.Linkedin);
-    app.set('user_var', req.body.User);
-    app.set('github_var', req.body.Github);
+  var text = req.body.User;       
+  var pattern = "vitbhopal.ac.in";
+  var count = 0;
 
-    if (req.body.PASSWORD == "fourmuskeeters" || req.body.PASSWORD == 100229) {
-        res.redirect("/chat");
-
-    }
-    else {
-        res.redirect("/error")
-    }
-})
-
+  // Lgic behind vitbhopal domain login only
+  for(let j=text.length - 15; j<text.length; j++){
+    if(text[j]!=pattern[count]){
+        break;
+      }
+      count++;
+  }
+  if(count==pattern.length){
+    res.redirect("/chat");
+  }else{
+    res.redirect("/error");
+  }
+});
 
 app.post("/error", function (req, res) {
-    res.redirect("/")
-})
-
+  res.redirect("/");
+});
 
 //server created
 const server = app.listen(process.env.PORT || 3000, function () {
-    console.log("Server running on PORT 3000.");
-})
+  console.log("Server running on PORT 3000.");
+});
 
 //socket io connections
-const io = require('socket.io')(server,{
-    cors:{
-        origin:"*"
-    }
-})
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
-io.on('connection',(socket) =>{
-    console.log("Welcome to Web-Connect " + socket.id)
+io.on("connection", (socket) => {
+  console.log("Welcome to Web-Connect " + socket.id);
 
-    socket.on('sent_message', data =>{
-        console.log(data)
-        socket.broadcast.emit('sent_message', data)
-    })
-
-})
+  socket.on("sent_message", (data) => {
+    console.log(data);
+    socket.broadcast.emit("sent_message", data);
+  });
+});
 
 //Chatcord - under construction
 app.get("/chatcordLogin", function (req, res) {
-    res.render("chatcordLogin")
-})
+  res.render("chatcordLogin");
+});
 
-app.post("/chatcordLogin", function(req, res){
-  res.redirect("/chatcord")
-})
+app.post("/chatcordLogin", function (req, res) {
+  res.redirect("/chatcord");
+});
+
 app.get("/chatcord", function (req, res) {
-  res.render("chatcord")
-})
+  res.render("chatcord");
+});
 
-
-
+app.post("/chatcord", function (req, res) {
+  res.redirect("/chat");
+});
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-const botName = 'ChatCord Bot';
+const botName = "ChatCord Bot";
 
 // Run when client connects
-io.on('connection', socket => {
-  socket.on('joinRoom', ({ username, room }) => {
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
     const user = userJoin(socket.id, username, room);
 
     socket.join(user.room);
 
     // Welcome current user
-    socket.emit('message', formatMessage(botName, 'Welcome to Web-Connect Queries!'));
+    socket.emit(
+      "message",
+      formatMessage(botName, "Welcome to Web-Connect Queries!")
+    );
 
     // Broadcast when a user connects
     socket.broadcast
       .to(user.room)
       .emit(
-        'message',
+        "message",
         formatMessage(botName, `${user.username} has joined the chat`)
       );
 
     // Send users and room info
-    io.to(user.room).emit('roomUsers', {
+    io.to(user.room).emit("roomUsers", {
       room: user.room,
-      users: getRoomUsers(user.room)
+      users: getRoomUsers(user.room),
     });
   });
 
   // Listen for chatMessage
-  socket.on('chatMessage', msg => {
+  socket.on("chatMessage", (msg) => {
     const user = getCurrentUser(socket.id);
 
-    io.to(user.room).emit('message', formatMessage(user.username, msg));
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
   });
 
   // Runs when client disconnects
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     const user = userLeave(socket.id);
 
     if (user) {
       io.to(user.room).emit(
-        'message',
+        "message",
         formatMessage(botName, `${user.username} has left the chat`)
       );
 
       // Send users and room info
-      io.to(user.room).emit('roomUsers', {
+      io.to(user.room).emit("roomUsers", {
         room: user.room,
-        users: getRoomUsers(user.room)
+        users: getRoomUsers(user.room),
       });
     }
   });
